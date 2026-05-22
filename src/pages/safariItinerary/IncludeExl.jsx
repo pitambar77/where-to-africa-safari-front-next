73643726d1068ed0b6cb810340227d4c669a7ecb
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2026-05-22 15:28:56 +0530

    comment solve coo

--- src/pages/safariItinerary/IncludeExl.jsx
+++ src/pages/safariItinerary/IncludeExl.jsx
@@ -7,14 +7,14 @@ const IncludeExl = ({ aboutBooking = [], requirements = [] }) => {
       <div className="px-4 md:px-10 lg:px-16 xl:px-20 2xl:px-28 flex justify-center py-4 md:py-10">
         <IncluExcluContainer
           title="Packages Include"
-          items={aboutBooking}
+          items={aboutBooking || []}
         />
       </div>
 
       <div className="px-4 md:px-10 lg:px-16 xl:px-20 2xl:px-28 flex justify-center py-10">
         <IncluExcluContainer
           title="Packages Exclude"
-          items={requirements}
+          items={requirements || []}
         />
       </div>
     </>
